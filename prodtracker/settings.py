e60da2bb4a442fb805b03b1d2ed44fa96ac9c407
JohnRei <johnrei.enriquez@gmail.com>
2017-11-03 08:40:44 +0000

    removed unipath and added reqs

--- prodtracker/settings.py
+++ prodtracker/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 from decouple import config, Csv
-from unipath import Path
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
